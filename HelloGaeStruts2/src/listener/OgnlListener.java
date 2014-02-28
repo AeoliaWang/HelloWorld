121036852da6b6fbaad0e9286e52385869b1a08e
cyokodog <cyokodog@gmail.com>
2014-02-28 16:11:16 +0900

    add struts2 on google app engine

---
+++ HelloGaeStruts2/src/listener/OgnlListener.java
@@ -0,0 +1,17 @@
+package listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import ognl.OgnlRuntime;
+
+public class OgnlListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0){
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0){
+		OgnlRuntime.setSecurityManager(null);
+	}
+}
